f5af52e1f2bcd3abd2adba9bd09c33a320a3d6ae
alma-project <61327478+alma-project@users.noreply.github.com>
2020-03-18 08:40:26 +0100

    Update items.py

--- items.py
+++ items.py
@@ -87,7 +87,7 @@ class Robot:
         logitems += [
             f"failed to assemble foobar; {bar} restored"
             for bar in collectedrsrc.bars
-            if completejob.jtype == JobType.SELL_ASSEMBLEBAR
+            if completejob.jtype == JobType.SELL_ASSEMBLEFOOBAR
         ]
         logitems += [
             f"{foobar} assembled" for foobar in collectedrsrc.foobars
